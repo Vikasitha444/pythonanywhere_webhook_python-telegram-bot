eb759f88fc353a336d54522d7560282d14460892
Vikasitha444 <79461283+Vikasitha444@users.noreply.github.com>
2022-04-08 20:33:15 +0530

    Update pythonanywhere_webhook_for_python-telegram-bot.py

--- pythonanywhere_webhook_for_python-telegram-bot.py
+++ pythonanywhere_webhook_for_python-telegram-bot.py
@@ -1,73 +1,11 @@
-import logging
-
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
 
 
 api_key = "asdasdasdasda5465as4d6a5s4d65as";
 server_url = "https://pawanvikasitha.pythonanywhere.com/";
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(api_key)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-
-    #setting up the webhook
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=api_key)
-    updater.bot.setWebhook(server_url_with_https,api_key)
-
-
-
-if __name__ == '__main__':
-    main()
+#setting up the webhook - place this code under the def main()-> none
+updater.start_webhook(listen="0.0.0.0",
+                      port=int(PORT),
+                      url_path=api_key)
+updater.bot.setWebhook(server_url_with_https,api_key)
